57f39ebfb027d8ffa5a6d18ee642b0513928a72f
Aiden <a.izumi@haulmont.com>
2022-02-09 10:01:17 +0400

    Solve the problem

--- src/main/java/org/izumi/jmix/replaceentity/screen/substitution/SubstitutionEdit.java
+++ src/main/java/org/izumi/jmix/replaceentity/screen/substitution/SubstitutionEdit.java
@@ -1,10 +1,24 @@
 package org.izumi.jmix.replaceentity.screen.substitution;
 
+import io.jmix.core.usersubstitution.CurrentUserSubstitution;
+import io.jmix.ui.component.TextField;
 import io.jmix.ui.screen.*;
 import org.izumi.jmix.replaceentity.entity.Substitution;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @UiController("Substitution.edit")
 @UiDescriptor("substitution-edit.xml")
 @EditedEntityContainer("substitutionDc")
 public class SubstitutionEdit extends StandardEditor<Substitution> {
+
+    @Autowired
+    private TextField<String> usernameField;
+
+    @Autowired
+    private CurrentUserSubstitution currentUserSubstitution;
+
+    @Subscribe
+    private void onBeforeShow(BeforeShowEvent event) {
+        usernameField.setValue(currentUserSubstitution.getEffectiveUser().getUsername());
+    }
 }
